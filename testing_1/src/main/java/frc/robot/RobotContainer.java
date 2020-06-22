74468dc2062e9f9c5d86c9105da97b8a2e0a07b2
nicolasfung2004 <nicolasfung2004@yahoo.com.hk>
2020-06-22 13:10:42 +0800

    test

--- testing_1/src/main/java/frc/robot/RobotContainer.java
+++ testing_1/src/main/java/frc/robot/RobotContainer.java
@@ -18,6 +18,8 @@ import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.commands.DriveCommand;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.FeederSubsystem;
+import frc.robot.commands.FeederUpCommand;
+import frc.robot.commands.FeederDownCommand;
 import frc.robot.subsystems.ShooterSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -54,7 +56,7 @@ public class RobotContainer {
    */
   public RobotContainer() {
     //Set the default commands here. Configure the button bindings here.
-    m_driveSubsystem.setDefaultCommand(new DriveCommand(m_driveSubsystem, () -> m_stick.getThrottle(), () -> -m_stick.getY(), () -> m_stick.getX()));
+    m_driveSubsystem.setDefaultCommand(new DriveCommand(m_driveSubsystem, /*() ->*/ m_stick.getThrottle(), /*() ->*/ -m_stick.getY(), /*() ->*/ m_stick.getX()));
 
     configureButtonBindings();
   }
--- testing_1/src/main/java/frc/robot/commands/FeederUpCommand.java
+++ testing_1/src/main/java/frc/robot/commands/FeederUpCommand.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,43 +7,36 @@
 
 package frc.robot.commands;
 
-import java.util.function.DoubleSupplier;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.FeederSubsystem;
 
-public class DriveCommand extends CommandBase {
+public class FeederUpCommand extends CommandBase {
 
   //Create local variables for the class here.
-  private static DriveSubsystem m_driveSubsystem;
-  private static Double/*Supplier*/ m_scale;
-  private static Double/*Supplier*/ m_speed;
-  private static Double/*Supplier*/ m_rotation;
+  private static FeederSubsystem m_feederSubsystem;
 
   //Define the local variables to be the value of the instance variables.
-  public DriveCommand(DriveSubsystem driveSubsystem, Double/*Supplier*/ scale, Double/*Supplier*/ speed, Double/*Supplier*/ rotation) {
-    m_driveSubsystem = driveSubsystem;
-    m_scale = scale;
-    m_speed = speed;
-    m_rotation = rotation;
+  public FeederUpCommand(FeederSubsystem feederSubsystem) {
+    m_feederSubsystem = feederSubsystem;
 
-    addRequirements(m_driveSubsystem);
+    addRequirements(m_feederSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_driveSubsystem.resetEncoders();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute(){
-    m_driveSubsystem.drive(-m_speed/*.getAsDouble()*/, m_rotation/*.getAsDouble()*/, m_scale/*.getAsDouble()*/);
+  public void execute() {
+    m_feederSubsystem.feederUp();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    m_feederSubsystem.feederStop();
   }
 
   // Returns true when the command should end.
